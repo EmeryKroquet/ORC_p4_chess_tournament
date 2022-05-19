0e66f27b5628a72ecea836a195823a2980af280f
EmeryKroquet <52417677+EmeryKroquet@users.noreply.github.com>
2022-05-19 18:32:29 +0200

    add requirements.txt

--- Models/classes/tournament.py
+++ Models/classes/tournament.py
@@ -1,4 +1,74 @@
 from Models.classes import Player
-from Models.classes import Round 
-from Models.classes import Match 
+from Models.classes import Round
+from Models.classes import Match
 
+
+def get_matches(players, opponents):
+    """recursive function to get matches for round 2 and more"""
+    if len(players) % 2 == 1:
+        return []
+    if len(players) == 0:
+        return []
+    first_player = players[0]
+    for player in players[1:]:
+        if not player[0] in opponents[first_player[0]]:
+            pair = (first_player, player)
+            remaining_players = players[1:]
+            remaining_players.remove(player)
+            pairs = get_matches(remaining_players, opponents)
+            if pairs is not None:
+                return [pair] + pairs
+
+
+class Tournament:
+    """ Modele réprésentant un tournoi"""
+
+    def __init__(
+        self, id, name, date, time_control, description,
+        total_number_of_rounds=4, ongoing_round=1,
+        players=None, status='initialisation',
+        list_of_rounds=None, scores=None, opponents=None
+    ):
+
+        if opponents is not None:
+            opponents = {}
+        if scores is not None:
+            scores = {}
+        if players is not None:
+            players = []
+        if list_of_rounds is not None:
+            list_of_rounds = []
+
+        self.id = id
+        self.name = name
+        self.date = date
+        self.time_control = time_control
+        self.description = description
+        self.total_number_of_rounds = total_number_of_rounds
+        self.ongoing_round = ongoing_round
+        self.players = players
+        self.status = status
+        self.list_of_rounds = list_of_rounds
+        self.scores = scores
+        self.opponents = opponents
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def serialize(self):
+        """sérialiser le tournoi"""
+        seliazed_tournament = {
+            'id': self.id,
+            'name': self.name,
+            'date': self.date,
+            'time_control': self.time_control,
+            'description': self.description,
+            'total_number_of_rounds': self.total_number_of_rounds,
+            'ongoing_round': self.ongoing_round,
+            'palyers': self.players,
+            'status': self.status,
+            'list_of_rounds': self.list_of_rounds,
+            'scores': self.scores,
+            'oppents': self.opponents
+        }
+        return seliazed_tournament
